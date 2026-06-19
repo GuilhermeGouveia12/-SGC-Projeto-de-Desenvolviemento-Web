d5f230bde78d787d23eeea836df2b55a3c5edb2b
Arthur Grangeiro <184882740+ArthurGrangeiro@users.noreply.github.com>
2026-06-18 21:21:44 -0300

    chore: migra para SQLite com dados de demonstração

--- sgc/settings.py
+++ sgc/settings.py
@@ -59,12 +59,8 @@ WSGI_APPLICATION = 'sgc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':     config('DB_NAME',     default='sgc'),
-        'USER':     config('DB_USER',     default='postgres'),
-        'PASSWORD': config('DB_PASSWORD', default=''),
-        'HOST':     config('DB_HOST',     default='localhost'),
-        'PORT':     config('DB_PORT',     default='5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
